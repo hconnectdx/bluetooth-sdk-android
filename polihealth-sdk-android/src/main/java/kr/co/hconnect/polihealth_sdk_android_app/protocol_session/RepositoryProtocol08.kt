f53c3c730d38bd3bf5839900f577f093538c1e41
곽민우 <kmwdev@hconnect.co.kr>
2024-07-08 13:41:35 +0900

    Feat: 패지키 이름 변경 후 삭제 #1

--- polihealth-sdk-android/src/main/java/kr/co/hconnect/polihealth_sdk_android_app/protocol_session/RepositoryProtocol08.kt
+++
@@ -1,7 +0,0 @@
-package kr.co.hconnect.polihealth_sdk_android_app.protocol_session
-
-import android.util.Log
-import java.io.File
-import java.io.FileOutputStream
-
-object RepositoryProtocol08 : ByteController()
